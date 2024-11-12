6644ced5d0eddd72199141d370438917fea7e053
GVS SRI RAM <gvs.iitkgp@gmail.com>
2024-11-12 14:40:20 -0500

    tqdm

--- pytorch_wrapper.sh
+++ pytorch_wrapper.sh
@@ -4,7 +4,8 @@ eval "$(/home/$(whoami)/miniconda3/bin/conda shell.bash hook)"
 
 conda activate pytorch
 
-conda install --yes --file requirements.txt
+# conda install --yes --file requirements.txt
 # pip install -r requirements.txt
+conda install tqdm==4.66.6
 
 python3 Notebooks/conditional_diffusion_model_unet_pytorch.py
