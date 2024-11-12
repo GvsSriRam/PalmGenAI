0d11565cd06f0c2787fb8abdacb4a930ebbaa7f1
GVS SRI RAM <gvs.iitkgp@gmail.com>
2024-11-12 14:29:28 -0500

    requirements_sh

--- pytorch_wrapper.sh
+++ pytorch_wrapper.sh
@@ -4,4 +4,7 @@ eval "$(/home/$(whoami)/miniconda3/bin/conda shell.bash hook)"
 
 conda activate pytorch
 
+# conda install --yes --file requirements.txt
+pip install -r requirements.txt
+
 python3 Notebooks/conditional_diffusion_model_unet_pytorch.py
