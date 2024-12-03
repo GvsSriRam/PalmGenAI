75225a8f1f70c015768794193abb3b3cd20aae34
GVS SRI RAM <gvs.iitkgp@gmail.com>
2024-12-03 05:17:18 -0500

    weminin_wrapper

--- pytorch_wrapper.sh
+++ pytorch_wrapper.sh
@@ -4,4 +4,4 @@ eval "$(/home/$(whoami)/miniconda3/bin/conda shell.bash hook)"
 
 conda activate pytorch
 
-python3 Notebooks/conditional_diffusion_model_unet_pytorch_non_linear.py
+python3 Notebooks/gemini.py
