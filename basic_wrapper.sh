16033704a1f2deb24cc3d550b3d4233cc3614e88
GVS SRI RAM <gvs.iitkgp@gmail.com>
2024-12-10 17:20:00 -0500

    cleanup

--- basic_wrapper.sh
+++ basic_wrapper.sh
@@ -4,4 +4,4 @@ eval "$(/home/$(whoami)/miniconda3/bin/conda shell.bash hook)"
 
 conda activate pytorch
 
-python3 Notebooks/basic.py
+python3 Notebooks/basic_diffusion_model_128.py
--- unet_wrapper.sh
+++ unet_wrapper.sh
@@ -4,4 +4,4 @@ eval "$(/home/$(whoami)/miniconda3/bin/conda shell.bash hook)"
 
 conda activate pytorch
 
-python3 Notebooks/basic_backup_resized.py
+python3 Notebooks/basic_unet_model_128.py
